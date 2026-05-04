dd15f567ffb6e8c88dbda5b8005a053bb1945836
Мария Овсянникова <oatmeal.00@mail.ru>
2026-05-04 18:36:45 +0500

    fix: исправить редирект главной страницы

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -1,5 +1,7 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/docs/intro" />;
+  return <Redirect to="/docs/docs/intro" />;
 }
